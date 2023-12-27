b9fbd521aa138b1092fcadecb78f66a745d353a2
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2023-12-27 16:01:12 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='invest',
-      version='0.0.40',
+      version='0.1.0',
       description='A collection of utilities for investors and traders',
-      url='https://github.com/AlessandroGianfelici/pyInvest.git',
+      url='https://github.com/AlessandroGianfelici/scikit-invest.git',
       author='Alessandro Gianfelici',
       author_email='alessandro.gianfelici@hotmail.com',
       license='MIT License',
@@ -11,7 +11,7 @@ setup(name='invest',
       include_package_data=True,
       install_requires=['numpy',
                         'pandas',
-                        #'yfinance',
+                        'yahooquery',
                         'plotly',
                         'datetime'],
       zip_safe=False)
