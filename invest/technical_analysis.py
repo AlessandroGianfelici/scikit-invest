b29080e2fb6a4b3da8d02789581feb04a810ecd6
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2024-03-05 10:19:25 +0100

    removed all the annoying warnings from the trend detector

--- invest/technical_analysis.py
+++ invest/technical_analysis.py
@@ -27,9 +27,9 @@ def detect_trend(full_hist, train_length = 120, verbose=True):
     y_linear = (clean_trend['Close']/norm_price).apply(np.log).values.reshape(-1, 1)
     
     ts_model = TheilSenRegressor()
-    ts_model.fit(x_linear, y_linear)
+    ts_model.fit(x_linear, y_linear.ravel())
     
-    current_trend = full_hist.loc[full_hist.index > best_b]
+    current_trend = full_hist.loc[full_hist.index > best_b].copy()
     x_predict = (current_trend.index/data_norm).values.reshape(-1, 1)
     current_trend['predicted_trend'] = np.squeeze(ts_model.predict(x_predict))
     current_trend['predicted_trend'] = current_trend['predicted_trend'].apply(np.exp)*norm_price
@@ -37,8 +37,9 @@ def detect_trend(full_hist, train_length = 120, verbose=True):
     projection = np.exp(ts_model.predict([[(365+data_norm)/data_norm]]))
     actual = np.exp(ts_model.predict([[(data_norm)/data_norm]]))
     trend_magnitude = (projection - actual)/actual
+    trend_duration = len(current_trend)
     if verbose:
         pw_fit.summary()
         piecewise_regression_results(pw_fit)
         plot_candle(current_trend, trendline).show()
-    return trend_magnitude.item(), current_trend.tail(1)['predicted_trend'].item()
+    return trend_magnitude.item(), current_trend.tail(1)['predicted_trend'].item(), trend_duration
